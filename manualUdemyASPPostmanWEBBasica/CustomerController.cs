bac51eadbc2fe8bb810a24388807045639c2510e
hugobepa <hugo_bcn@hotmail.com>
2019-02-18 20:38:57 +0100

    Manual propiocurso

---
+++ manualUdemyASPPostmanWEBBasica/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindApiDemo.Models;
+using NorthwindApiDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindApiDemo.Controllers
+{
+    [Route("api/customers")]
+    public class CustomerController : Controller
+    {
+        private ICustomerRepository _customerRepository;
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        [HttpGet()]
+        public IActionResult GetCustomers()
+        {
+            var customers =
+                _customerRepository
+                .GetCustomers();
+            var results = Mapper
+                .Map<IEnumerable<CustomerWithoutOrders>>(customers);
+            return new JsonResult(results);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCustomer(string id, bool includeOrders = false)
+        {            
+            var customer =
+                _customerRepository.GetCustomer(id, includeOrders);
+            if(customer == null)
+            {
+                return NotFound();
+            }
+         if(includeOrders)
+            {
+                var customerResult =
+                    Mapper.Map<CustomerDTO>(customer);
+                return Ok(customerResult);
+            }
+            var customerResultOnly =
+                   Mapper.Map<CustomerWithoutOrders>(customer);
+            return Ok(customerResultOnly);            
+        }
+    }
+}
---
+++ manualUdemyASPPostmanWEBBasica/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using NorthwindApiDemo.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindApiDemo.Services
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customers> GetCustomers();
+        Customers GetCustomer(string customerId, bool includeOrders);
+        IEnumerable<Orders> GetOrders(string customerId);
+        Orders GetOrder(string customerId, int orderId);
+        bool CustomerExists(string customerId);
+        void AddOrder(string customerId, Orders order);
+        bool Save();
+        void DeleteOrder(Orders order);
+    }
+}
